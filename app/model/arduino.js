3557e38e7ecdce391053ceb09bd18f8bab97514c
UsielOM <usiel31197@outlook.es>
2022-07-05 21:37:55 -0500

    Metodo para conectar al arduino

--- app/model/arduino.js
+++ app/model/arduino.js
@@ -1,3 +1,14 @@
+const serialPort = require('serialport');
 module.exports = function() {
+    const port = new serialPort('COM5', { baudRate: 9600 });
+
+    const parser = new serialPort.parser.Readline();
+
+    port.pipe(parser);
+
+    parser.on('data', (line) => {
+        console.log("El arduino dice: ", line);
+        port.write('Era una vez');
+    });
 
 }
