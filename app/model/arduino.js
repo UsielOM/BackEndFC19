9cfbd580cad7c4b1665421f3f0866fae7681497c
UsielOM <usiel31197@outlook.es>
2022-07-07 11:58:02 -0500

    Implementando codigo nuevo para el serial port

--- app/model/arduino.js
+++ app/model/arduino.js
@@ -2,16 +2,14 @@ const { SerialPort } = require('serialport')
 module.exports = function() {
 
     const port = new SerialPort({ path: 'COM6', baudRate: 9600 })
+    port.on('open', function() {
+        console.log('Conecion establecida correctamente');
 
-    port.write('main screen turn on', function(err) {
-        if (err) {
-            return console.log('Error on write: ', err.message)
-        }
-        console.log('message written')
+    });
+    port.on('data', function(data) {
+        console.log('Data:', data);
+        port.write(`Arduino dice:  ${data}`);
     })
 
-    // Open errors will be emitted as an error event
-    port.on('error', function(err) {
-        console.log('Error: ', err.message)
-    })
+
 }
